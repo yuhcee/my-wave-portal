dce1d16f35fb6578aba190080d8c30eae7929366
yuhcee <egbouchenna001@gmail.com>
2022-03-05 23:58:44 +0100

    emit new wave

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -34,12 +34,18 @@ contract WavePortal {
     function wave (string memory _message) public {
         totalWaves += 1;
         console.log("%s waved w/ message %s", msg.sender, _message);
-    }
 
     /*
      * This is where I actually store the wave data in the array.
     */
-    waves.push(Wave(msg.sender, _message, block.timestamp));
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+/*
+         * I added some fanciness here, Google it and try to figure out what it is!
+         * Let me know what you learn in #general-chill-chat
+         */
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
 
     function getTotalWaves() public view returns(uint256) {
         console.log("We have %d total waves!", totalWaves);
